a12652a886a35ec966c6d445bee8ad482876e1d9
Mikiyas Tibebu <mikitebe123@gmail.com>
2024-04-05 20:13:00 +0300

    configure shadcn and astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), db()]
+});
